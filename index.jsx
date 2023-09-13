8765a38b3dec6e3c9e90e4da93331caff3e3fb09
nicetomeetor <nicetomeetor@gmail.com>
2023-09-14 02:32:05 +0300

    make basic server

--- index.jsx
+++ index.jsx
@@ -1,5 +1,31 @@
 import * as React from 'react'
 import * as Server from 'react-dom/server'
-import App from './src/App';
+import * as App from './src/App';
 
-console.log(Server.renderToString(<App />))
+
+import * as Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', async function handler (request, reply) {
+    const app = Server.renderToString(<App />);
+
+    const html = `
+        <html>
+        <body>
+            <div id="root">${app}</div>
+        </body>
+        </html>
+    `
+
+    reply.status(200).type('text/html').send(html)
+})
+
+try {
+    fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
